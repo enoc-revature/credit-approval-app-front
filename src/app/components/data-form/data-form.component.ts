1e27728abe52cc5f808959ae2623ee8c9d2e65f1
Enoc Santiago <enocsanti2@gmail.com>
2020-04-29 10:06:11 -0400

    Page finished, no validation, 30% test coverage

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -35,115 +35,31 @@ export class DataFormComponent implements OnInit {
     daysAlive: new FormControl('')
   });
 
-  //   get name(): number {
-  //   // return this.dataForm.get("daysEmployed");
-  //   return 0;
-  // }
-  dateToDays(d: FormControl): number {
-    d.setValue('');
-    return 0;
+  dateToDays(d): number {
+      let currentTime = new Date().getTime();
+      let givenTime = new Date(d).getTime();
+      return Math.floor((currentTime - givenTime)/1000/60/60/24);
+  }
+
+  boolToY(b) {
+    if(b==true) //Not just truthy, but specifically true.
+      return "Y";
+    else
+      return "N";
   }
 
   onSubmit(): void {
     if (this.dataForm.valid) {
       console.log("Form Submitted!");
-      let currentDays = new Date().getTime();
-      let daysEmployed = new Date(this.dataForm.value.daysEmployed).getTime();
-      let daysAlive = new Date(this.dataForm.value.daysAlive).getTime();
-      this.dataForm.value.daysEmployed = Math.floor((currentDays - daysEmployed)/1000/60/60/24);
-      this.dataForm.value.daysAlive = Math.floor((currentDays - daysAlive)/1000/60/60/24);
+
+      this.dataForm.value.daysEmployed = this.dateToDays(this.dataForm.value.daysEmployed);
+      this.dataForm.value.daysAlive = this.dateToDays(this.dataForm.value.daysAlive);
+
+      this.dataForm.value.ownProperty = this.boolToY(this.dataForm.value.ownProperty);
+      this.dataForm.value.ownCar = this.boolToY(this.dataForm.value.ownCar);
+
       this.dss.postData(this.dataForm.value).subscribe(approved => {this.approved = approved;})
       //this.dataForm.reset();
+    }
   }
-  }
-  /*
-    Field order and grouping:
-
-      Employment Info:
-      incomeTotal
-      incomeType
-      occupationType
-      daysEmployed
-
-      Family Info:
-      familyStatus
-      numChildren
-      familyMembers
-
-      Property Info
-      housingType
-      ownProperty
-      ownCar
-
-      Miscellaneous:
-      educationType
-      daysAlive
-  */
-
-
-
-
-
-
-  /* Selection inputs
-     NAME_INCOME_TYPE:
-    'Working', 'Commercial associate', 'Pensioner', 'State servant', 'Student' (edited) 
-
-    NAME_EDUCATION_TYPE:
-    Higher education
-    Secondary / secondary special
-    Incomplete higher
-    Lower secondary
-    Academic degree
-
-    NAME_FAMILY_STATUS:
-    Civil marriage
-    Married
-    Single / not married
-    Separated
-    Widow
-
-    NAME_HOUSING_TYPE:
-    Rented apartment
-    House / apartment
-    Municipal apartment
-    With parents
-    Co-op apartment
-    Office apartment
-
-    OCCUPATION_TYPE:
-    Security staff
-    Sales staff
-    Accountants
-    Laborers
-    Managers
-    Drivers
-    Core staff
-    High skill tech staff
-    Cleaning staff
-    Private service staff
-    Cooking staff
-    Low-skill Laborers
-    Medicine staff
-    Secretaries
-    Waiters/barmen staff
-    HR staff
-    Realty agents
-    IT staff
-
-    DAYS_BIRTH and DAYS_EMPLOYED are stored as days 
-
-
-    Vars to Drop
-    ID
-    CODE_GENDER
-    FLAG_MOBIL
-    FLAG_WORK_PHONE
-    FLAG_PHONE
-    FLAG_EMAIL
-
-    
-
-
-  */
 }
