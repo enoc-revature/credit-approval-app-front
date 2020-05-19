b0b7dd87c92fb670923e912c3c51dd90b630b7aa
Enoc Santiago <enocsanti2@gmail.com>
2020-05-19 11:14:34 -0400

    Moved data manipulation in component to service

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -47,32 +47,32 @@ export class DataFormComponent implements OnInit {
   //   marital_status: new FormControl('')
   // });
 
-  modifyFormGroup(form: FormGroup): string{
-    form.value.purpose = {"0":form.value.purpose};
-    form.value.cred_hist = {"0":form.value.cred_hist};
-    form.value.employment_hist = {"0":form.value.employment_hist};
-    form.value.guarantors = {"0":form.value.guarantors};
-    form.value.property = {"0":form.value.property};
-    form.value.other_debt = {"0":form.value.other_debt};
-    form.value.employment_curr = {"0":form.value.employment_curr};
-    form.value.phone = {"0":form.value.phone};
-    form.value.foreign_worker = {"0":form.value.foreign_worker};
-    form.value.gender = {"0":form.value.gender};
-    form.value.marital_status = {"0":form.value.marital_status};
-    form.value.housing = {"0":form.value.housing};
-    form.value.checking = {"0":form.value.checking};
-    form.value.savings = {"0":form.value.savings};
+  // modifyFormGroup(form: FormGroup): string{
+  //   form.value.purpose = {"0":form.value.purpose};
+  //   form.value.cred_hist = {"0":form.value.cred_hist};
+  //   form.value.employment_hist = {"0":form.value.employment_hist};
+  //   form.value.guarantors = {"0":form.value.guarantors};
+  //   form.value.property = {"0":form.value.property};
+  //   form.value.other_debt = {"0":form.value.other_debt};
+  //   form.value.employment_curr = {"0":form.value.employment_curr};
+  //   form.value.phone = {"0":form.value.phone};
+  //   form.value.foreign_worker = {"0":form.value.foreign_worker};
+  //   form.value.gender = {"0":form.value.gender};
+  //   form.value.marital_status = {"0":form.value.marital_status};
+  //   form.value.housing = {"0":form.value.housing};
+  //   form.value.checking = {"0":form.value.checking};
+  //   form.value.savings = {"0":form.value.savings};
 
-    // Numeric values
-    form.value.residence = {"0":parseInt(form.value.residence)};
-    form.value.term_months = {"0":parseInt(form.value.term_months)};
-    form.value.amount = {"0":parseInt(form.value.amount)};
-    form.value.age = {"0": parseInt(form.value.age)};
-    form.value.household_size = {"0":parseInt(form.value.household_size)};
-    form.value.cred_this_bank = {"0":parseInt(form.value.cred_this_bank)};
+  //   // Numeric values
+  //   form.value.residence = {"0":parseInt(form.value.residence)};
+  //   form.value.term_months = {"0":parseInt(form.value.term_months)};
+  //   form.value.amount = {"0":parseInt(form.value.amount)};
+  //   form.value.age = {"0": parseInt(form.value.age)};
+  //   form.value.household_size = {"0":parseInt(form.value.household_size)};
+  //   form.value.cred_this_bank = {"0":parseInt(form.value.cred_this_bank)};
 
-    return JSON.stringify(form.value);
-  }
+  //   return JSON.stringify(form.value);
+  // }
 
   boolToString(b): string {
     if(b==true) //Not just truthy, but specifically true.
@@ -84,21 +84,22 @@ export class DataFormComponent implements OnInit {
   onSubmit(): void {
     let output: string;
     if (this.formInput.valid) {
-      console.log("Form Submitted!");
+      // console.log("Form Submitted!");
 
-      console.log("Form Before:");
-      console.log(this.formInput.value);
-      this.formInput.value.phone = this.boolToString(this.formInput.value.phone);
-      this.formInput.value.foreign_worker = this.boolToString(this.formInput.value.foreign_worker);
-      output = this.modifyFormGroup(this.formInput);
+      // console.log("Form Before:");
+      // console.log(this.formInput.value);
+      // this.formInput.value.phone = this.boolToString(this.formInput.value.phone);
+      // this.formInput.value.foreign_worker = this.boolToString(this.formInput.value.foreign_worker);
+      // output = this.modifyFormGroup(this.formInput);
 
-      console.log("Form After:");
-      console.log(this.formInput.value);
+      // console.log("Form After:");
+      // console.log(this.formInput.value);
 
-      this.dss.postData(output).subscribe(approved => {
+      // this.dss.postData(output).subscribe(approved => {
+      this.dss.postData(this.formInput).subscribe(approved => {
         this.approved = approved;
-        console.log("Result from postData().subscribe():");
-        console.log(approved);
+        // console.log("Result from postData().subscribe():");
+        // console.log(approved);
         let result = approved["result"];
         if(result === 1.0)
           this.showSuccess = true;
