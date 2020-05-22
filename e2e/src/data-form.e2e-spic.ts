9f5951a2e3039ca77696f34a53280425ceaaac77
Enoc Santiago <enocsanti2@gmail.com>
2020-05-22 09:50:55 -0400

    Skipping unit tests on component for now.  Moving on to e2e testing branch.

--- e2e/src/data-form.e2e-spic.ts
+++ e2e/src/data-form.e2e-spic.ts
@@ -1,38 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
-import { DataFormComponent } from './data-form.component';
-import { DataSubmissionService } from 'src/app/services/data-submission.service';
-import { RESULT_TRUE,RESULT_FALSE } from 'src/mock-data/results';
-import { FORM_DATA_B1,FORM_DATA_B2 } from 'src/mock-data/formDataBeforeService';
-import { FORM_DATA_A1,FORM_DATA_A2 } from 'src/mock-data/formDataAfterService';
-import { element, by, $ } from 'protractor';
-
-describe('DataFormComponent', () => {
-  let component: DataFormComponent;
-  let fixture: ComponentFixture<DataFormComponent>;
-
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ DataFormComponent ],
-      imports: [ HttpClientTestingModule ],
-      providers: [ DataSubmissionService ]
-    })
-    .compileComponents();
-  }));
-
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DataFormComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-
-  fit('test input fields to formGroup', () => {
-    
     // String
     // let elem = document.getElementById("#purposeId");
     // console.log(elem.);
@@ -60,5 +26,3 @@ describe('DataFormComponent', () => {
     // // Boolean
     // document.getElementById("#foreign_workerId").innerHTML = FORM_DATA_B1.value.foreign_worker;
     // document.getElementById("#phoneId").innerHTML = FORM_DATA_B1.value.phone;
-  });
-});
