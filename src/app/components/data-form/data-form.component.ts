4d2692031c8924a2b7b7c4ef4d092622891b6fa7
Enoc Santiago <enocsanti2@gmail.com>
2020-05-22 10:30:10 -0400

    forgot to make new branch

--- src/app/components/data-form/data-form.component.ts
+++ src/app/components/data-form/data-form.component.ts
@@ -23,7 +23,6 @@ export class DataFormComponent implements OnInit {
   showDenied = false;
 
   //TODO: add validation code to form
-  // formInput = {...formInputTemplate}; // Tried to change.
   formInput = formInputTemplate;
   // formInput = new FormGroup({
   //   checking: new FormControl(''),
@@ -48,39 +47,6 @@ export class DataFormComponent implements OnInit {
   //   marital_status: new FormControl('')
   // });
 
-  // modifyFormGroup(form: FormGroup): string{
-  //   form.value.purpose = {"0":form.value.purpose};
-  //   form.value.cred_hist = {"0":form.value.cred_hist};
-  //   form.value.employment_hist = {"0":form.value.employment_hist};
-  //   form.value.guarantors = {"0":form.value.guarantors};
-  //   form.value.property = {"0":form.value.property};
-  //   form.value.other_debt = {"0":form.value.other_debt};
-  //   form.value.employment_curr = {"0":form.value.employment_curr};
-  //   form.value.phone = {"0":form.value.phone};
-  //   form.value.foreign_worker = {"0":form.value.foreign_worker};
-  //   form.value.gender = {"0":form.value.gender};
-  //   form.value.marital_status = {"0":form.value.marital_status};
-  //   form.value.housing = {"0":form.value.housing};
-  //   form.value.checking = {"0":form.value.checking};
-  //   form.value.savings = {"0":form.value.savings};
-
-  //   // Numeric values
-  //   form.value.residence = {"0":parseInt(form.value.residence)};
-  //   form.value.term_months = {"0":parseInt(form.value.term_months)};
-  //   form.value.amount = {"0":parseInt(form.value.amount)};
-  //   form.value.age = {"0": parseInt(form.value.age)};
-  //   form.value.household_size = {"0":parseInt(form.value.household_size)};
-  //   form.value.cred_this_bank = {"0":parseInt(form.value.cred_this_bank)};
-
-  //   return JSON.stringify(form.value);
-  // }
-
-  // boolToString(b): string {
-  //   if(b==true) //Not just truthy, but specifically true.
-  //     return "yes";
-  //   else
-  //     return "no";
-  // }
 
   popupOfResult(approved: JSON): void {
         let result = approved["result"];
@@ -94,27 +60,8 @@ export class DataFormComponent implements OnInit {
   onSubmit(): void {
     let output: string;
     if (this.formInput.valid) {
-      // console.log("Form Submitted!");
-
-      // console.log("Form Before:");
-      // console.log(this.formInput.value);
-      // this.formInput.value.phone = this.boolToString(this.formInput.value.phone);
-      // this.formInput.value.foreign_worker = this.boolToString(this.formInput.value.foreign_worker);
-      // output = this.modifyFormGroup(this.formInput);
-
-      // console.log("Form After:");
-      // console.log(this.formInput.value);
-
       // this.dss.postData(output).subscribe(approved => {
       this.dss.postData(this.formInput).subscribe(approved => {
-        // this.approved = approved;
-        // console.log("Result from postData().subscribe():");
-        // console.log(approved);
-        // let result = approved["result"];
-        // if(result === 1.0)
-        //   this.showSuccess = true;
-        // else
-        //   this.showDenied = true;
         this.popupOfResult(approved);
       })
       this.formInput.reset();
